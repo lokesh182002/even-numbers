13e7292ff928b5efb294533a56fd1c2fcf3ab0df
lokesh pemmanaboina <84330548+lokesh182002@users.noreply.github.com>
2021-08-18 21:39:11 +0530

    Add files via upload

---
+++ even numbers from 1 to n.py
@@ -0,0 +1,9 @@
+#python script to print all even numbers from 1 to n.
+def even():
+    n=int(input("enter value:"))
+    for i in range(1,n+1):
+        if (i%2==0):
+            print(i)
+        
+        
+          
